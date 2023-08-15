0ebd5e1ed26cf4efcf1a0ab145da55a01d4bd88c
mainaanthony <mainaanthonymwai@gmail.com>
2023-08-15 09:38:23 +0300

    Began building a gymzilla project I have started with the landing page and populating the necessary containers I'll need

---
+++ src/Landing/landingPage.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='main-container'>
+     
+     <div className='top-container'>
+       <div className='top-left-container'>
+
+       </div>
+
+       <div className='top-right-corner'>
+
+       </div>
+         
+     </div>
+
+     <div className='ourProducts-container'>
+
+        <div className='ourProducts-left-container'>
+
+        </div>
+        <div className='ourProducts-right-container'>
+
+        </div>
+
+     </div>
+     <div className='gymProducts-container'>
+
+        <div className='gymProducts-left-container'>
+
+        </div>
+        <div className='gymProducts-right-container'>
+
+        </div>
+
+     </div>
+
+     <div className='nutritionProducts-container'>
+
+        <div className='nutritionProducts-left-container'>
+
+        </div>
+        <div className='nutritionProducts-right-container'>
+
+        </div>
+
+     </div>
+
+     <div className='nutritionProducts-container'>
+
+        <div className='nutritionProducts-left-container'>
+
+        </div>
+        <div className='nutritionProducts-right-container'>
+
+        </div>
+
+     </div>
+
+     <div className='classes-container'>
+     <div className='classes-left-container'>
+            <div className='gymClassImage'>
+
+            </div>
+              <h2>Gym Classes</h2>
+         </div>
+     <div className='classes-right-container'>
+     <div className='nutritionClassImage'>
+
+       </div>
+         <h2>Nutrition Classes</h2>
+               
+           </div>
+            
+     </div>
+
+     
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default LandingPage
